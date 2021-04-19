ec8a0717352ed5a841fd64f9eba40a481df05eac
erdem besler <erdem.besler94@gmail.com>
2021-04-19 21:03:26 +0300

    navbar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,11 +10,12 @@ const Navbar = ({ click }) => {
 
       <ul className="navbar__links">
         <li>
-          <i className="fas fa-shopping-cart"></i>
-
-          <Link to="/cart">
-            Cart
-            <span className="cartlogo__badge">0</span>
+          <Link to="/cart" className="cart__link">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart
+              <span className="cartlogo__badge">0</span>
+            </span>
           </Link>
         </li>
         <li>
