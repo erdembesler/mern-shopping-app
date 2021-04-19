f82207333fc6033e7604f8785904b6af3f5c73bd
erdem besler <erdem.besler94@gmail.com>
2021-04-19 23:43:24 +0300

    Home screen and product component with mock data

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,22 @@
 import "./HomeScreen.css";
 
+import Product from "../components/Product";
 const HomeScreen = () => {
-  return <div className="homescreen">HomeScreen</div>;
+  return (
+    <div className="homescreen">
+      <h2 className="homescree__title">
+        <div className="homescreen__products">
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </div>
+      </h2>
+    </div>
+  );
 };
 
 export default HomeScreen;
